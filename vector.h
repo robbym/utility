d396bac1299565aea13192143ea577f78d0e312c
Robby Madruga <robbymadruga@gmail.com>
2017-07-30 17:31:06 -0700

    Added vector sort

--- vector.h
+++ vector.h
@@ -69,6 +69,32 @@ void vector_remove_##T(vector_##T* self, size_t idx) {\
         memmove(&self->data[idx], &self->data[idx+1], sizeof(T) * (self->len - idx - 1));\
         self->len--;\
     }\
-}
+}\
+\
+static T* partition(T* left, T* right, int (*compare) (T*, T*)) {\
+    T* pivot = left;\
+    left--;\
+    right++;\
+    for (;;) {\
+        do {left++;} while (compare(left, pivot) < 0);\
+        do {right--;} while (compare(right, pivot) > 0);\
+        if (left >= right) return right;\
+        T swap = *left;\
+        *left = *right;\
+        *right = swap;\
+    }\
+}\
+\
+static void quicksort(T* left, T* right, int (*compare) (T*, T*)) {\
+    if (left < right) {\
+        T* p = partition(left, right, compare);\
+        quicksort(left, p, compare);\
+        quicksort(p+1, right, compare);\
+    }\
+}\
+\
+void vector_sort_##T(vector_##T* self, int (*compare) (T*, T*)) {\
+    quicksort(&self->data[0], &self->data[self->len - 1], compare);\
+}\
 
 #endif
